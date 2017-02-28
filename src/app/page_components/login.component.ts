c07dbadc585e577e43e73798fb9cb8835680b250
Lorenzo Gagliani <lorenz_g@hotmail.it>
2017-02-28 10:20:54 +0100

    responsie login and signup

--- src/app/page_components/login.component.ts
+++ src/app/page_components/login.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "login-page",
     templateUrl: "app/pages/login-page.template.html",
-    styleUrls: ['app/css/login-page.css']
+    styleUrls: ['app/css/login-signup-page.css']
 })
 export class LoginPageComponent {
 
--- src/app/page_components/signup.component.ts
+++ src/app/page_components/signup.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: "signup-page",
   templateUrl: "/app/pages/signup-page.template.html",
-  styleUrls: ['app/css/signup-page.css']
+  styleUrls: ['app/css/login-signup-page.css']
 })
 export class SignUpPageComponent {
 
