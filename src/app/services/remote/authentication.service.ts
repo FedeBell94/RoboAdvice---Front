c4dce79803514c2f256911f1a5260fb2f1fd5be5
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-03-06 14:27:53 +0100

    added autologin

--- src/app/services/remote/authentication.service.ts
+++ src/app/services/remote/authentication.service.ts
@@ -30,23 +30,9 @@ export class AuthService {
         }).map((res)=>{
             if (res.response > 0) {
                 this.setAuthToken(res.data.userToken);
-                this.logged = true;
                 //if i'm correctly logged, i'll set the default headerOptions for remote calls to pass the auth token on each request
-                let headers = new Headers;
-                headers.append('Accept', 'application/json');
-                headers.append('Content-Type', 'application/json');
-                headers.append('Access-Control-Allow-Credentials', 'true');
-                headers.append('User-Token', this.getAuthToken());
-                let options = new RequestOptions({ headers: headers });
-                this.apis.setDefaultRequestOptions(options);
-                //now i save the user
-                this.currentUser = new User();
-                this.currentUser.email = res.data.user.email;
-                this.currentUser.id = res.data.user.id;
-                this.currentUser.username = res.data.user.username;
-                //if the user has a username
-                if (this.currentUser.username) this.router.navigate(["/dashboard"]);
-                else this.router.navigate(["/survey"]);
+                this.setAuthHeaders();
+                this.logInWithUser(res.data.user);
             }
             return res;
         });
@@ -76,7 +62,17 @@ export class AuthService {
     }
 
     getAuthToken() {
-        return this.estractCookie("authToken")
+        return this.estractCookie("authToken");
+    }
+
+    checkSession() {
+        this.setAuthHeaders();
+        return this.apis.get("tellMeWhoAmI").map((res: any)=> {
+            if (res.response > 0) {
+                this.logInWithUser(res.data);
+            }
+            return res;
+        });
     }
 
     private estractCookie(cookieName: string) {
@@ -85,5 +81,32 @@ export class AuthService {
         for (let i = 0; i < parts.length; i++) {
             if (parts[i] && parts[i].split("=")[0].trim() == cookieName) return parts[i].split("=")[1];
         }
+    }
+
+    private setAuthHeaders() {
+        let headers = new Headers;
+        headers.append('Accept', 'application/json');
+        headers.append('Content-Type', 'application/json');
+        headers.append('Access-Control-Allow-Credentials', 'true');
+        headers.append('User-Token', this.getAuthToken());
+        let options = new RequestOptions({ headers: headers });
+        this.apis.setDefaultRequestOptions(options);
+    }
+
+    private saveUser(user: any) {
+        this.currentUser = new User();
+        this.currentUser.email = user.email;
+        this.currentUser.id = user.id;
+        this.currentUser.username = user.username;
+    }
+
+    private logInWithUser(user: any) {
+        this.saveUser(user);
+        this.logged = true;//if i'm correctly logged, i'll set the default headerOptions for remote calls to pass the auth token on each request
+        //now i save the user
+        this.saveUser(user);
+        //if the user has a username
+        if (this.currentUser.username) this.router.navigate(["/dashboard"]);
+        else this.router.navigate(["/survey"]);
     }
 }
