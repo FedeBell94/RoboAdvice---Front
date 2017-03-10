67c926aeb9766eec18b8e1ef16e58c8d782f91f9
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-03-10 16:38:00 +0100

    restored autologin

--- src/app/services/remote/authentication.service.ts
+++ src/app/services/remote/authentication.service.ts
@@ -32,6 +32,7 @@ export class AuthService {
             if (res.response > 0) {
                 //if i'm correctly logged, i'll set the default headerOptions for remote calls to pass the auth token on each request
                 this.setAuthHeaders();
+                this.setAuthToken(btoa(email + ":" + password));
                 this.logInWithUser(res.data);
             }
             return res;
@@ -68,11 +69,17 @@ export class AuthService {
     }
 
     checkSession() {
-        this.setAuthHeaders();
-        return this.apis.get("tellMeWhoAmI").map((res: any)=> {
+        let headers = new Headers();
+        headers.append("Authorization","Basic " + this.getAuthToken());
+        let opts = new RequestOptions({ headers: headers, withCredentials: true });
+
+        return this.apis.post("loginUser", {}, opts).map((res)=>{
             if (res.response > 0) {
+                //if i'm correctly logged, i'll set the default headerOptions for remote calls to pass the auth token on each request
+                this.setAuthHeaders();
                 this.logInWithUser(res.data);
             }
+            console.log("BAD CRED");
             return res;
         });
     }
@@ -88,7 +95,7 @@ export class AuthService {
         let value = "; " + document.cookie;
         let parts: any = value.split(";");
         for (let i = 0; i < parts.length; i++) {
-            if (parts[i] && parts[i].split("=")[0].trim() == cookieName) return parts[i].split("=")[1];
+            if (parts[i] && parts[i].split("=")[0].trim() == cookieName) return parts[i].split("=")[1] + "=";
         }
     }
 
