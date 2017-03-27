8f8a801a3d1caadde8aa2b3063a442c02b2483aa
ciccarellipasquale <ciccarellipa@gmail.com>
2017-03-27 16:15:52 +0200

    get recommended strategy method inside strategy service

--- src/app/services/strategy/strategy.service.ts
+++ src/app/services/strategy/strategy.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { ApiService } from '../remote/remote-call/remote-call.service';
 import { Strategy } from '../../model/strategy/strategy';
+import {ForecastingService} from "../forecasting/forecasting.service";
+import {isNumber} from "util";
 
 @Injectable()
 export class StrategyService {
@@ -13,4 +15,45 @@ export class StrategyService {
     saveStrategy(strategy: Strategy) {
         return this.apis.post('strategy', strategy.asset_class);
     }
+    getRecommendedStrategy(rawForecastData: ForecastData[]): Strategy{
+        let deltaArray: number[]=[];
+        for( let i=0; i<rawForecastData.length; i++){
+            if( (i+1) == rawForecastData[i].assetClassId){
+                deltaArray[i] = 0 - rawForecastData[i].value;
+            }else{
+                break;
+            }
+        }
+        let min: number = 0;
+        let sum: number = 0;
+        let suggestStrategy: Strategy = new Strategy();
+        for( let i=0; i<deltaArray.length; i++){
+          deltaArray[i] += rawForecastData[rawForecastData.length-deltaArray.length].value;
+          min = (deltaArray[i]<min) ? deltaArray[i] : min;
+        }
+        for( let i=0; i<deltaArray.length; i++){
+          deltaArray[i] += min;
+          sum += deltaArray[i];
+        }
+        suggestStrategy.name = "Recommended Strategy";
+        for( let i=0; i<deltaArray.length; i++){
+          suggestStrategy.asset_class[i].assetClassId = i+1;
+          suggestStrategy.asset_class[i].percentage= Math.round(100*deltaArray[i]/sum);
+
+        }
+        return suggestStrategy;
+    }
+
+}
+
+
+export class ForecastData{
+  constructor(){
+
+  }
+
+  value : number;
+  data : string;
+  assetClassId: number;
+
 }
