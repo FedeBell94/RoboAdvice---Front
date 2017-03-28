b3d8097f99b2d93716750990535f122b5ea0076c
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-03-28 17:10:42 +0200

    registration has now validation

--- src/app/views/register/register.component.ts
+++ src/app/views/register/register.component.ts
@@ -1,47 +1,74 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
-import {AuthService} from "../../services/remote/authentication.service";
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Router } from "@angular/router";
+import { AuthService } from "../../services/remote/authentication.service";
 
 
 @Component({
-    selector: 'register',
-    templateUrl: 'register.template.html',
-    styleUrls: ['register.style.css']
+  selector: 'register',
+  templateUrl: 'register.template.html',
+  styleUrls: ['register.style.css']
 })
-export class registerComponent implements OnInit{
+export class registerComponent implements OnInit {
 
   constructor(
     private auth: AuthService,
     private router: Router,
-  ) {  }
+  ) { }
 
-    ngOnInit(){
+  @ViewChild('email') email: ElementRef;
+  @ViewChild('nickname') username: ElementRef;
+  @ViewChild('pwd') password: ElementRef;
 
-    }
+  private formIsValid = false;
+  ngOnInit() {
+    this.validateAll();
+  }
+  
+  validateAll() {
+    this.formIsValid = 
+        this.validateEmail(this.email.nativeElement.value) &&
+        this.validateUsername(this.username.nativeElement.value) &&
+        this.validatePassword(this.password.nativeElement.value);
+  }
 
-    submitRegistration(email:string, nickname: string, pwd: string) {
-        this.auth.register(email, nickname, pwd).subscribe(data => {
-            if (data.response > 0) {
-              (window as any).swal(
-                'Registration successful!',
-                'Let\'s make money',
-                'success'
-              )
-              this.router.navigate(["/login"]);
-            } else {
-              (window as any).swal(
-                'Oops...',
-                'Something went wrong!',
-                'error'
-              );
-                switch (data.errorCode) {
-                    case 100:
-                        console.log("registration Failed");
-                        break;
-                    default:
-                        break;
-                }
-            }
-        });
-    }
+  validateEmail(val: string) {
+    let re = /([a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?)/;
+    return re.test(val);
+  }
+
+  validateUsername(val: string) {
+    let re = /^[a-zA-Z0-9\s]+$/;
+    return (val.length > 4 && re.test(val) && val.length < 16);
+  }
+
+  validatePassword(val: string) {
+    let re = /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{6,16}$/;
+    return re.test(val);
+  }
+
+  submitRegistration(email: string, nickname: string, pwd: string) {
+    this.auth.register(email, nickname, pwd).subscribe(data => {
+      if (data.response > 0) {
+        (window as any).swal(
+          'Registration successful!',
+          'Let\'s make money',
+          'success'
+        )
+        this.router.navigate(["/login"]);
+      } else {
+        (window as any).swal(
+          'Oops...',
+          'Something went wrong!',
+          'error'
+        );
+        switch (data.errorCode) {
+          case 100:
+            console.log("registration Failed");
+            break;
+          default:
+            break;
+        }
+      }
+    });
+  }
 }
