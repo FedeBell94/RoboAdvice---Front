f29093b4bb59fa409af5b34fc3db25c3c1ecc41e
Fabio Brunori <fabio.brunori.94@gmail.com>
2017-03-29 15:06:14 +0200

    recompiled for prod
    build version is now handled
    every time a new version is detected, the cache is cleared (not the user one)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,56 +1,74 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { correctHeight, detectBody } from './app.helpers';
-import {Router} from "@angular/router";
+import { Router } from "@angular/router";
 
-import {AuthService} from "./services/remote/authentication.service";
+import { AuthService } from "./services/remote/authentication.service";
+import { RoboAdviceConfig } from "./app.configuration";
+import { LocalStorage } from "./annotations/local-storage.annotation";
+import { PortfolioService } from "./services/portfolio/portfolio.service";
+import { AssetService } from "./services/asset/asset.service";
+import { NeuralNetworkService } from "./services/forecasting/neural-network.service";
 
-declare var jQuery:any;
+declare var jQuery: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit{
+export class AppComponent implements OnInit {
 
-  constructor(
-      private auth: AuthService,
-      private router: Router,
-  ){ }
+    constructor(
+        private auth: AuthService,
+        private router: Router,
+        private portfolio: PortfolioService,
+        private asset: AssetService,
+        private neural: NeuralNetworkService,
+    ) { }
+    @LocalStorage() private cachedVersion: string;
+    private roboConfig = RoboAdviceConfig;
 
+    ngAfterViewInit() {
+        // Run correctHeight function on load and resize window event
+        jQuery(window).bind("load resize", function () {
+            correctHeight();
+            detectBody();
+        });
 
-  ngAfterViewInit() {
-    // Run correctHeight function on load and resize window event
-    jQuery(window).bind("load resize", function() {
-      correctHeight();
-      detectBody();
-    });
+        // Correct height of wrapper after metisMenu animation.
+        jQuery('.metismenu a').click(() => {
+            setTimeout(() => {
+                correctHeight();
+            }, 300)
+        });
+    }
 
-    // Correct height of wrapper after metisMenu animation.
-    jQuery('.metismenu a').click(() => {
-      setTimeout(() => {
-        correctHeight();
-      }, 300)
-    });
-  }
+    ngOnInit() {
+        //checking build version to check if we need to clear the cache
+        if (this.cachedVersion && this.cachedVersion != this.roboConfig.Build) {
+            this.portfolio.wipeCache();
+            this.asset.wipeCache();
+            this.neural.wipeCache();
+        }
+        //saving current version
+        this.cachedVersion = this.roboConfig.Build;
 
-  ngOnInit(){
-    this.auth.checkSession().subscribe((data)=> {
-      if (data.response > 0) {
-        this.router.navigate(["/mainView"]);
-      } else {
-        this.router.navigate(["/login"]);
-      }
-    });
-  }
+        this.auth.checkSession().subscribe((data) => {
+            if (data.response > 0) {
+                this.router.navigate(["/mainView"]);
+            } else {
+                this.router.navigate(["/login"]);
+            }
+        });
+    }
 
-  isLogged(){
-    return this.auth.isLogged();
-  }
+    isLogged() {
+        return this.auth.isLogged();
+    }
 
-  getUsername(): any {
-    if (this.auth.isLogged()) return this.auth.getUser().username;
-    return false;
-  }
+    getUsername(): any {
+        if (this.auth.isLogged()) return this.auth.getUser().username;
+        return false;
+    }
 
 }
